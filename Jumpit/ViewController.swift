fb08baf96013e7881a7697a14d87ac0be83b9d29
hooni <jhlee909090@gmail.com>
2024-05-08 14:23:02 +0900

    feat: 기본 세팅 완성
    스토리보드 삭제
    SceneDelegate에서 ViewController 로 시작

---
+++ Jumpit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jumpit
+//
+//  Created by 이지훈 on 5/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+
+}
+
