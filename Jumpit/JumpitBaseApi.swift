7a882ef4c64427d9aedff960f089110060530941
YIZIHN <jhlee909090@gmail.com>
2024-05-23 11:48:31 +0900

    Update Jumpit/JumpitBaseApi.swift
    Co-authored-by: wjdalswl <109158284+wjdalswl@users.noreply.github.com>

--- Jumpit/JumpitBaseApi.swift
+++ Jumpit/JumpitBaseApi.swift
@@ -8,25 +8,8 @@ import Foundation
 import Moya
 
 // MARK: - Domain
-enum JumpitAPIDomain {
-    case resumes
-    case positions
-}
-
-extension JumpitAPIDomain {
-    var url: String {
-        switch self {
-        case .resumes:
-            return "/resumes"
-        case .positions:
-            return "/positions"
-        }
-    }
-}
-
 protocol JumpitAPIEndpoint: TargetType {
-    var domain: JumpitAPIDomain { get }
-    var urlPath: String { get }
+    var path: String { get }
     var error: [Int: NetworkError]? { get }
 }
 
@@ -34,10 +17,7 @@ extension JumpitAPIEndpoint {
     var baseURL: URL {
         return URL(string: PrivacyInfoManager.baseURL)!
     }
-    
-    var path: String {
-        return domain.url + urlPath
-    }
+
     
     var validationType: ValidationType {
         return .successCodes
