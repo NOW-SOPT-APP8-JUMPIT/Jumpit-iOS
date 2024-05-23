7e13072de43e49e70add387cd7651806539744e3
hooni <jhlee909090@gmail.com>
2024-05-23 14:45:12 +0900

    🛠️Fix : 깃에서 잘못눌러서 outdatede 눌러버렷는데 이거 파일 원상복구했다 민지야

--- Jumpit/JumpitAPI.swift
+++ Jumpit/JumpitAPI.swift
@@ -17,52 +17,64 @@ enum JumpitAPI {
     case fetchPositionDetail(positionId: String)
 }
 
-extension JumpitAPI: JumpitBaseAPI {
-    var domain: JumpitAPIDomain {
-            return .resumes
+extension JumpitAPI: TargetType {
+    var baseURL: URL {
+        guard let urlString = Bundle.main.object(forInfoDictionaryKey: "BaseURL") as? String,
+              let url = URL(string: urlString) else {
+            fatalError("BaseURL not set in plist for this environment")
         }
+        return url
     }
 
     var path: String {
         switch self {
         case .postResume:
-            return ""
+            return "/resumes"
         case .fetchMyResume(let userId):
-            return "/\(userId)"
+            return "/resumes/\(userId)"
         case .toggleResumePrivacy(let resumeId):
-            return "/\(resumeId)"
+            return "/resumes/\(resumeId)"
+        case .searchPositions, .searchPositionsFiltered:
+            return "/positions"
+        case .fetchPositionDetail(let positionId):
+            return "/positions/\(positionId)"
         }
     }
-
     var method: Moya.Method {
-        switch self {
-        case .postResume:
-            return .post
-        case .fetchMyResume, .searchPositions, .searchPositionsFiltered, .fetchPositionDetail:
-            return .get
-        case .toggleResumePrivacy:
-            return .patch
+            switch self {
+            case .postResume:
+                return .post
+            case .fetchMyResume, .searchPositions, .searchPositionsFiltered, .fetchPositionDetail:
+                return .get
+            case .toggleResumePrivacy:
+                return .patch
+            }
+        }
+
+        var task: Task {
+            switch self {
+            case .postResume(let data):
+                return .requestJSONEncodable(data)
+            case .fetchMyResume, .fetchPositionDetail, .toggleResumePrivacy:
+                return .requestPlain
+            case .searchPositions(let keyword):
+                return .requestParameters(parameters: ["keyword": keyword], encoding: URLEncoding.queryString)
+            case .searchPositionsFiltered(let keyword, let categories):
+                return .requestParameters(parameters: ["keyword": keyword, "categories": categories], encoding: URLEncoding.queryString)
+            }
+        }
+
+        var headers: [String: String]? {
+            return ["Content-Type": "application/json"]
+        }
+
+        var validationType: ValidationType {
+            return .successCodes
         }
     }
 
-    var task: Task {
-        switch self {
-        case .postResume(let data):
-            return .requestJSONEncodable(data)
-        case .fetchMyResume, .fetchPositionDetail, .toggleResumePrivacy:
-            return .requestPlain
-        case .searchPositions(let keyword):
-            return .requestParameters(parameters: ["keyword": keyword], encoding: URLEncoding.queryString)
-        case .searchPositionsFiltered(let keyword, let categories):
-            return .requestParameters(parameters: ["keyword": keyword, "categories": categories], encoding: URLEncoding.queryString)
-        }
-    }
-
-    var headers: [String: String]? {
-        return ["Content-Type": "application/json"]
-    }
-
-    var validationType: ValidationType {
-        return .successCodes
+extension JumpitAPI {
+    var urlPath: String {
+        return baseURL.appendingPathComponent(path).absoluteString
     }
 }
--- Jumpit/NewworkProvider.swift
+++ Jumpit/NewworkProvider.swift
@@ -8,25 +8,24 @@
 import Foundation
 import Moya
 
-final class NetworkProvider<API: JumpitAPIEndpoint> {
-    
+final class NetworkProvider<API: TargetType> {
     private let provider: MoyaProvider<API>
-    
+
     init(plugins: [PluginType] = []) {
         self.provider = MoyaProvider(plugins: plugins)
     }
-    
+
     func request(api: API, completion: @escaping (Result<Response, MoyaError>) -> Void) {
-        let requestString = "\(api.urlPath)"
+        let requestString = "\(api.baseURL)\(api.path)"
         
         provider.request(api) { result in
             switch result {
             case let .success(response):
-                print("SUCCESS: \(requestString) (\(response.statusCode)) 이 떳다 민지야 잘했어!")
+                print("SUCCESS: \(requestString) (\(response.statusCode))")
                 completion(.success(response))
                 
             case let .failure(error):
-                print("ERROR: \(requestString) 가 떳다 민지야 수정하자")
+                print("ERROR: \(requestString)")
                 completion(.failure(error))
             }
         }
